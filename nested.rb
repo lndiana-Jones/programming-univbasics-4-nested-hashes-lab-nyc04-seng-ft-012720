a38691b66eba2f81da204be644043921f8906a74
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-01-23 19:24:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,13 +81,12 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
- }
+ 
      }
-  
-    
+  programmer_hash[:yukihiro_matsumoto] = {}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash
 end
 
 def changing_alan
